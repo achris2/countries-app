aa7b6a0e094c4bc7d39e9b67dafc689d9057b6e0
Mikael <copemikael@gmail.com>
2023-12-22 20:04:02 +0200

    Changes to the javascript to render countries and weather in the same card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 // global variables 
-let inputCountry = "United Kingdom"; 
 
 // insert HTML selectors here
 
@@ -12,6 +11,8 @@ $('#country-search').on('keypress', function (event) {
         event.preventDefault();
         let inputCountry= $('#country-search').val();
         console.log (inputCountry);
+        searchCountry(inputCountry)
+        
     }
 });
 
@@ -30,37 +31,30 @@ $('#country-search').on('keypress', function (event) {
 
 // pulling from RestCountries API 
 
-function searchCountry(){
+function searchCountry(inputCountry){
     const queryURLCountry = `https://restcountries.com/v3.1/name/${inputCountry}`;
     fetch(queryURLCountry)
     .then(function(response){
         return response.json();
-    }) .then (function(data){
-        console.log(data);
+    }) .then (function(results){
+        console.log(results);
         // flag
-        cardDiv.append(`
-                <div class="cardContainer col-lg-3 col-md-3 col-sm-12">
-            <div class="card" style="width: 18rem;">
-                <div class="card-body">
-                <img class="card-img-top" src="${data[0].flags.png}" alt="Card image cap">
-                <h5 class="card-title">${inputCountry}</h5>
-                <p class="card-text"><b>Capital: </b>${data[0].capital}</p>
-                <p class="card-text"><b>Population: </b>${data[0].population}</p>
-                <p class="card-text"><b>Region: </b>${data[0].region}</p>
-                <a href="#" class="btn btn-primary">Save to Favourites!</a>
-                </div>
-            </div>
-            </div>
-        
-        `);
+        var data = {
+            name: results[0].name.common,
+            flag: results[0].flags.png,
+            capital: results[0].capital,
+            population: results[0].population,
+            region: results[0].region
+        }
+        searchWeather(inputCountry, data)
 })
 }
 
-searchCountry();
+
 
 // pulling from Open Weather API 
 
-function searchWeather(){
+function searchWeather(inputCountry, data){
     // API Key for Open Weather 
 
     const weatherAPIKey = "fa4695e0608a76d517ec72dbb80b9028";
@@ -73,40 +67,74 @@ function searchWeather(){
     fetch(queryURLWeather)
     .then(function(response){
         return response.json();
-    }) .then (function(data){
-        console.log(data);
+    }) .then (function(results){
+        console.log(results);
 
-        let currentTime = new Date(data.list[0].dt * 1000);
+        let currentTime = new Date(results.list[0].dt * 1000);
 
         // format Date 
         let formattedDate = ('0' + currentTime.getDate()).slice(-2) + ' /' + ('0' + (currentTime.getMonth() + 1)).slice(-2) + ' /' + currentTime.getFullYear();
         
         //  Temperature 
-        let tempCelsius = Math.round(data.list[0].main.temp-273.15); Math.round()
+        let tempCelsius = Math.round(results.list[0].main.temp-273.15); Math.round()
 
         // Humidity 
 
-        let humidity = data.list[0].main.humidity;
+        let humidity = results.list[0].main.humidity;
         console.log(humidity);
 
         // Icon -         //Icon URL
         // https://openweathermap.org/img/w/' + weather[0].icon + '.png
-        let icon = data.list[0].weather[0].icon;
+        let icon = results.list[0].weather[0].icon;
         let iconURL = 'https://openweathermap.org/img/w/' + icon + '.png'; 
         console.log(icon);
         console.log(iconURL);
 
+        data = {
+            ...data,
+            date: formattedDate,
+            icon: iconURL,
+            temp: tempCelsius,
+            humidity: humidity
 
+        }
+
+        console.log(data)
+
+        renderCard(data)
         // appends the info on chosen city to main card 
-        cardDiv.append(`<p>${inputCountry} (${formattedDate}) <img src="${iconURL}"> </p>`);
-        cardDiv.append(`<p>Temp: ${tempCelsius} °C</p>`);
-        cardDiv.append(`<p>Humidity: ${humidity}%</p>`);
 
+        // <div class="WeatherDiv"></div>
+        // <a href="#" class="btn btn-primary">Save to Favourites!</a>
 })
 
 }
 
-searchWeather();
+function renderCard(data){
+    var cardBody = $('<div>')
+    cardBody.addClass('card-body')
+    cardBody.append(`
+         <img class="card-img-top" src="${data.flag}" alt="Card image cap">
+         <h5 class="card-title">${data.name}</h5>
+         <p class="card-text"><b>Capital: </b>${data.capital}</p>
+         <p class="card-text"><b>Population: </b>${data.population}</p>
+         <p class="card-text"><b>Region: </b>${data.region}</p>
+         <p>${data.name} (${data.date}) <img src="${data.icon}"> </p>
+         <p>Temp: ${data.temp} °C</p>
+         <p>Humidity: ${data.temp}%</p>
+         <a href="#" class="btn btn-primary">Save to Favourites!</a>
+    `)
+
+    cardDiv.append(`
+        <div class="cardContainer col-lg-3 col-md-3 col-sm-12">
+            <div class="card" style="width: 18rem;">
+                ${cardBody.html()}
+            </div>
+     </div>
+    
+    `);
+
+}
 
 // // pulling from Ninja Country API
 
